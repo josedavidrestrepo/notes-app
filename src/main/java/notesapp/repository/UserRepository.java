89217e4c03c6d8d5d7e992d9a40bb52c6688dea5
josedavidrestrepo <jodrestrepodu@unal.edu.co>
2018-03-09 00:42:39 -0500

    Fix deploy issues

--- src/main/java/notesapp/repository/UserRepository.java
+++ src/main/java/notesapp/repository/UserRepository.java
@@ -2,7 +2,9 @@ package notesapp.repository;
 
 import notesapp.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     User findById(String id);
